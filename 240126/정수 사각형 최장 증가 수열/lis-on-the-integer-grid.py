86cdb3fc0647f4fcec9a2c6c6b6bb19cf19d6836
Jo_on <jjuny9798@gmail.com>
2024-01-26 14:26:22 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 205ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240126/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240126/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,6 +1,7 @@
 # 각 칸에 적혀있는 수가 증가하는 순서대로 진행해야한다 -> 조건
 # 각 칸에 적혀있는 수들 중에서 작은값부터 순서대로 dp 값을 갱신
 # 칸에 적혀있는 수가 작은 것부터 오름차순 정렬하여 순서대로 dp값을 갱신!
+global n
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
@@ -24,15 +25,13 @@ for num, x, y in pos:
         if nx<0 or nx>=n or ny<0 or ny>=n:
             continue
         if graph[nx][ny]>num:
-            dp[nx][ny]+=1
+            dp[nx][ny] = dp[x][y]+1
 
 def find_max(dp):
-    global n
     max_num = 0
     for x in range(n):
         for y in range(n):
             if dp[x][y] > max_num:
                 max_num = dp[x][y]
     return max_num
-
 print(find_max(dp))
