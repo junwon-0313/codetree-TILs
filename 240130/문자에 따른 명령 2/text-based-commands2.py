71239e457d4cc83eccfd470ffd2da47f6328e6e1
Jo_on <jjuny9798@gmail.com>
2024-01-30 18:23:24 +0900

    Title: 문자에 따른 명령 2, Time: 125ms, Memory: 26MB, Status: Passed - CodeTree

--- 240130/문자에 따른 명령 2/text-based-commands2.py
+++ 240130/문자에 따른 명령 2/text-based-commands2.py
@@ -1,1 +1,17 @@
-x, y =
+# L: 반시계, R: 시계
+# 동 남 서 북
+dx, dy = [1,0,-1,0], [0,-1,0,1]
+lst = list(input())
+
+x,y, dir_num = 0, 0, 3
+for i in lst:
+    if i =='R':
+        dir_num = (dir_num+1)%4
+
+    elif i=='L':
+        dir_num = (dir_num-1)%4
+
+    elif i =='F':
+        x, y = x+dx[dir_num], y+dy[dir_num]
+        
+print(x,y)
