bec2c9ebf2dbeec3bdd505ec1fd0ee4101216b25
Jo_on <jjuny9798@gmail.com>
2024-02-07 14:27:20 +0900

    Title: 되돌아오기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/되돌아오기/come-back.py
@@ -0,0 +1,22 @@
+N = int(input())
+move_lst = [list(input().split()) for _ in range(N)]
+
+x, y = 0, 0
+#  동 서 남 북
+direction = dict()
+direction['E'] = [1,0]
+direction['W'] = [-1,0]
+direction['S'] = [0,1]
+direction['N'] = [0,-1]
+cnt = 0
+for di, dis in move_lst:
+    for x in range(int(dis)):
+        cnt +=1
+        x, y = x+direction[di][0], y+direction[di][1]
+        if x==0 and y==0:
+            print(cnt)
+            break
+    if (x,y)==(0,0):
+        break
+if (x,y)!=(0,0):
+    print(-1)
