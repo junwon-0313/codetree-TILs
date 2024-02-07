778dc85f2ffe910b0fedc0b76694619614fd4e41
Jo_on <jjuny9798@gmail.com>
2024-02-07 15:37:03 +0900

    Title: 거울에 레이저 쏘기 2, Time: 250ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240207/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,50 @@
+global N
+N = int(input())
+graph = [list(input()) for _ in range(N)]
+K = int(input())
+# 아, 오, 위, 왼 
+dx, dy = [1,0,-1,0], [0,1,0,-1]
+if K<=N:
+    x,y = 0, K-1
+    di= 0 
+elif N<K<=2*N:
+    x,y = K%N-1, N-1
+    di =1
+elif 2*N<K<=3*N:
+    x, y = N-1, N-1-(K%N)+1
+    di = 2
+else:
+    x, y =  N-1-(K%N)+1, 0
+    di =3
+
+def in_range(x, y):
+    return 0<=x<N and 0<=y<N
+
+cnt = 0
+while True:
+    # 거울에 부딪힘
+    cnt+=1
+    if graph[x][y]=='/':
+        if di ==0:
+            di=3
+        elif di==1:
+            di=2
+        elif di==2:
+            di=1
+        elif di==3:
+            di=0
+
+    elif graph[x][y]=='\\':
+        if di ==0:
+            di=1
+        elif di==1:
+            di=0
+        elif di==2:
+            di=3
+        elif di==3:
+            di=2
+    nx, ny = x+dx[di], y+dy[di]
+    if not in_range(nx,ny):
+        print(cnt)
+        break
+    x,y = nx, ny
