8c522d37f4291b8559e54292c5b9a784c605e89f
Jo_on <jjuny9798@gmail.com>
2024-02-10 23:19:20 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240210/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240210/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -1,28 +1,22 @@
 n = int(input())
-x, y = n//2, n//2
-# 오, 위, 왼, 아
-dx, dy = [0,-1,0,1],[1,0,-1,0]
-# 1 1 2 2 3 3 4 4
-cnt = []
-for i in range(1,n+1):
-    cnt.append(i)
-    cnt.append(i)
-
-num,idx =1, 0
+# 왼 위 오 아
+dx, dy = [0,-1,0,1],[-1,0,1,0]
+num,idx =n*n, 0
+x, y = n-1,n-1
 graph=[[0]*n for _ in range(n)]
-# 초기값
-graph[x][y]=num
-for i in cnt:
-    for j in range(i):
-        num+=1
-        # 이동 후
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+for cnt in range(n*n):
+    # 값을 할당
+    graph[x][y]= num-cnt
+    nx, ny = x+dx[idx], y+dy[idx]
+    if in_range(nx,ny) and graph[nx][ny]==0:
+        x,y= nx,ny
+    else:
+        idx= (idx+1)%4
         x, y = x+dx[idx], y+dy[idx]
-        # 범위를 넘어가면 종료
-        if x<0 or x>=n or y<0 or y>=n:
-            break
-        # 값을 넣어주기
-        graph[x][y]=num
-    idx= (idx+1)%4
 
 for x in range(n):
     for y in range(n):
