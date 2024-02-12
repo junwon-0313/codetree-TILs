860760965ce82bd1f9f11c4058146d69c8221b4e
Jo_on <jjuny9798@gmail.com>
2024-02-12 22:57:38 +0900

    Title: 격자로 사각형 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240212/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -0,0 +1,17 @@
+n = int(input())
+graph = [[0]*n for _ in range(n)]
+def initialize():
+    for x in range(5):
+        graph[x][0]=1
+        graph[0][x]=1
+
+initialize()
+for x in range(1,n):
+    for y in range(1,n):
+        graph[x][y]= graph[x-1][y]+ graph[x][y-1] + graph[x-1][y-1]
+
+
+for x in range(n):
+    for y in range(n):
+        print(graph[x][y],end=' ')
+    print()
