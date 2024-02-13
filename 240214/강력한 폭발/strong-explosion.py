ff6057c16424f7e5bc978fe393d6ff70facc78b2
Jo_on <jjuny9798@gmail.com>
2024-02-14 02:46:53 +0900

    Title: 강력한 폭발, Time: 439ms, Memory: 29MB, Status: Passed - CodeTree

--- 240214/강력한 폭발/strong-explosion.py
+++ 240214/강력한 폭발/strong-explosion.py
@@ -1,33 +1,33 @@
-# 초토화시키는 지역의 최대화
-total = []
-# 폭탄 10개
+total = [] # 최종 초토화된 영역의 수를 저장
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-# 폭탄 위치
-bomb_pos = []
+bomb_pos = [] # 폭탄 위치
 for i in range(n):
     for j in range(n):
         if graph[i][j]==1:
             bomb_pos.append((i,j))
 
+# 출력 함수
 def print_map(graph):
     for i in range(n):
         for j in range(n):
             print(graph[i][j], end =' ')
         print()
 
+# 초토화된 영역의 개수 파악
 def count(m):
     ans =0
     for i in range(n):
         for j in range(n):
-            if m[i][j]==0:
+            if m[i][j]==1:
                 ans+=1
-    return n**2-ans
+    return ans
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
+# 행렬 복사함수
 def copy(graph):
     new_graph = [[0]*n for _ in range(n)]
     for i in range(n):
@@ -40,27 +40,24 @@ def bomb1(graph, x, y):
     for idx in range(4):
         nx, ny = x+dx[idx], y+dy[idx]
         if in_range(nx, ny) and graph[nx][ny]!=1:
-            graph[nx][ny]=2
+            graph[nx][ny]=1
 def bomb2(graph, x, y):
     dx, dy = [-1,1,0,0],[0,0,-1,1]
     for idx in range(4):
         nx, ny = x+dx[idx], y+dy[idx]
         if in_range(nx, ny) and graph[nx][ny]!=1:
-            graph[nx][ny]=2
+            graph[nx][ny]=1
 
 def bomb3(graph, x, y):
     dx, dy = [-1,-1,1,1],[-1,1,-1,1]
     for idx in range(4):
         nx, ny = x+dx[idx], y+dy[idx]
         if in_range(nx, ny) and graph[nx][ny]!=1:
-            graph[nx][ny]=2
+            graph[nx][ny]=1
 
 # 재귀 함수
-# x,y 위치에서 폭탄 터트리기
 def bomb(graph, num):
-    # print('@@', num)
-    # print_map(graph)
-    # 종료 조건
+    # 종료 조건: 폭탄이 모두 한번씩 터진 후
     if num == len(bomb_pos):
         total.append(count(graph))
         return
