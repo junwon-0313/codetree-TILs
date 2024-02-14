2183aca9efc35e05a9ed4b96f21b293a61e539da
Jo_on <jjuny9798@gmail.com>
2024-02-14 17:59:34 +0900

    Title: 강력한 폭발, Time: 561ms, Memory: 29MB, Status: Passed - CodeTree

--- 240214/강력한 폭발/strong-explosion.py
+++ 240214/강력한 폭발/strong-explosion.py
@@ -16,11 +16,11 @@ def print_map(graph):
         print()
 
 # 초토화된 영역의 개수 파악
-def count(m):
+def count():
     ans =0
     for i in range(n):
         for j in range(n):
-            if m[i][j]==1:
+            if graph[i][j]==1:
                 ans+=1
     return ans
 
@@ -35,49 +35,35 @@ def copy(graph):
             new_graph[i][j]=graph[i][j]
     return new_graph
 
-# 첫번째 폭발
-def bomb1(graph, x, y):
-    dx, dy = [-2,-1,1,2],[0,0,0,0]
-    for idx in range(4):
-        nx, ny = x+dx[idx], y+dy[idx]
-        if in_range(nx, ny) and graph[nx][ny]!=1:
-            graph[nx][ny]=1
-# # 두번째 폭발
-def bomb2(graph, x, y):
-    dx, dy = [-1,1,0,0],[0,0,-1,1]
-    for idx in range(4):
-        nx, ny = x+dx[idx], y+dy[idx]
-        if in_range(nx, ny) and graph[nx][ny]!=1:
-            graph[nx][ny]=1
-# 세번째 폭발
-def bomb3(graph, x, y):
-    dx, dy = [-1,-1,1,1],[-1,1,-1,1]
+# 폭발
+def simulate_bomb(x, y, bomb_type):
+    dxdy_set = [
+        ([-2,-1,1,2],[0,0,0,0]),
+        ([-1,1,0,0],[0,0,-1,1]),
+        ([-1,-1,1,1],[-1,1,-1,1])
+    ]
+    dx, dy = dxdy_set[bomb_type]
     for idx in range(4):
         nx, ny = x+dx[idx], y+dy[idx]
         if in_range(nx, ny) and graph[nx][ny]!=1:
             graph[nx][ny]=1
 
 # 재귀 함수
-def bomb(graph, num):
+def bomb(num):
+    global graph
+    
     # 종료 조건: 폭탄이 모두 한번씩 터진 후
     if num == len(bomb_pos):
-        total.append(count(graph))
+        total.append(count())
         return
     x, y = bomb_pos[num]
-    # 폭탄 터지는 방법 1
-    new_graph = copy(graph) # 새로운 배열을 만들어서 전달
-    bomb1(new_graph,x,y)
-    bomb(new_graph,num+1)
 
-    # 폭탄 터지는 방법 2
-    new_graph = copy(graph)
-    bomb2(new_graph,x,y)
-    bomb(new_graph,num+1)
+    # 폭탄 터지는 시뮬레이션
+    for bomb_type in range(3):
+        temp = copy(graph) # 새로운 배열을 만들기
+        simulate_bomb(x,y,bomb_type)
+        bomb(num+1)
+        graph = copy(temp) # 다시 되돌리기
 
-    # 폭탄 터지는 방법 3
-    new_graph = copy(graph)
-    bomb3(new_graph,x,y)
-    bomb(new_graph,num+1)
-
-bomb(graph, 0)
+bomb(0)
 print(max(total))
