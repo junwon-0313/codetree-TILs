2cf96e774c7d544737dbf0d6f8f2f3db31265f6c
Jo_on <jjuny9798@gmail.com>
2024-02-15 16:15:08 +0900

    Title: xor 결과 최대 만들기, Time: 1155ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240215/xor 결과 최대 만들기/max-of-xor.py
+++ 240215/xor 결과 최대 만들기/max-of-xor.py
@@ -22,16 +22,19 @@ def xor():
     for num in choose:
         bin_lst.append(dec2bin(num))
     # 자릿수 맞춰주기
-    max_pos = max(len(bin_lst[0]), len(bin_lst[1]), len(bin_lst[2]))
-    for idx in range(3):
+    max_pos = max([len(idx) for idx in bin_lst])
+    for idx in range(m):
         if len(bin_lst[idx])<max_pos:
             bin_lst[idx] = '0'*(max_pos-len(bin_lst[idx]))+bin_lst[idx]
     max_bin = ''
     for pos in range(max_pos):
-        if bin_lst[0][pos] == bin_lst[1][pos] == bin_lst[2][pos]: # 다 같을 경우
-            max_bin+='0'
+        for idx in range(m-1):
+            if bin_lst[idx][pos] != bin_lst[idx+1][pos]:
+                max_bin+='1'
+                break
         else:
-            max_bin+='1'
+            max_bin+='0'
+            
     max_dec = bin2dec(max_bin)
     return max_dec
 
@@ -39,8 +42,12 @@ ans =0
 choose = []
 def comb(k, cnt):
     global ans
-    if cnt==3: # 출력
-        max_dec = xor()
+    if cnt== m: # 출력
+        # print('xor')
+        if m==1:
+            max_dec = choose[0]
+        else:
+            max_dec = xor()
         if max_dec >ans:
             ans = max_dec
     if k==n: # 종료 조건
