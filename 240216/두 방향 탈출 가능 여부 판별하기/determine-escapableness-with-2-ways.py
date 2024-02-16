1110ca484ff3cf923a3e94dc7e29860dd603df33
Jo_on <jjuny9798@gmail.com>
2024-02-16 16:44:40 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5017ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240216/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240216/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -2,19 +2,19 @@
 # 아래 -> 오른쪽
 # x,y: 행, 열
 # 이동, 방문체크 -> 격자안, 방문x, 조건: 뱀
-# 재귀 함수 호출 전에 방문처리!! -> 여기서는 방문처리 안해도됨
+# 재귀 함수 호출 전에 방문처리!!
 n,m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 dxs, dys = [1,0], [0,1]
+visited =[[False]*m for _ in range(n)]
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
-
 Final = False
 def dfs(x,y):
-    global Final
     if x==n-1 and y==m-1:
+        global Final
         Final= True
         return 
     for dx, dy in zip(dxs, dys):
@@ -23,8 +23,10 @@ def dfs(x,y):
             continue
         if graph[nx][ny]==0: # 뱀 만남
             continue
+        visited[nx][ny]=True
         dfs(nx,ny)
 
+visited[0][0]= True
 dfs(0,0)
 if Final:
     print(1)
