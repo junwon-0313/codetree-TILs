fa5d32dc1500d57d3c7048e7279a076d055cb66e
Jo_on <jjuny9798@gmail.com>
2024-02-16 20:24:14 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240216/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,36 @@
+# bfs: queue 자료구조
+# queue가 empty가 될 때까지 동작
+# 전부 방문, 재방문x
+from collections import deque
+
+n, m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+
+dxs, dys = [1,-1,0,0], [0,0,1,-1]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def bfs(start):
+    q= deque()
+    q.append(start)
+    while q:
+        x, y = q.popleft()
+        if x==n-1 and y==m-1:
+            return True
+        for dx,dy in zip(dxs, dys):
+            nx,ny =x+dx, y+dy
+            if not in_range(nx,ny) or visited[nx][ny] or graph[nx][ny]==0:
+                continue
+
+            visited[nx][ny]=True
+            q.append((nx,ny))
+    return False
+
+pos = bfs((0,0))
+
+if pos:
+    print(1)
+else:
+    print(0)
