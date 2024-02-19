c587f75d0e8ff499881a23aabedc1aba3e7b52e6
Jo_on <jjuny9798@gmail.com>
2024-02-19 16:28:22 +0900

    Title: 안전 지대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240219/안전 지대/comfort-zone.py
+++ 240219/안전 지대/comfort-zone.py
@@ -1,35 +1,33 @@
-# K 이하인 집들은 모두 비가 오면 잠김
-# 안전영역: 잠기지 않은 집들
-#-> 동일한 안전지역
-# 안전 영역의 수가 최대가 될 때의 K와 그 수
+# 최대 재귀 깊이를 설정
+import sys
+sys.setrecursionlimit(10**10)
+
 n,m = map(int, input().split())
-town = [list(map(int,input().split())) for _ in range(n)]
-dxs , dys = (0,0,1,-1), (1,-1,0,0)
+town = [list(map(int,input().split())) for _ in range(n)] # 집의 높이 graph
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
-def dfs(x,y,k): # 상하좌우
-    for dx, dy in zip(dxs,dys):
+dxs , dys = [0,0,1,-1], [1,-1,0,0]
+def dfs(x,y,k):
+    for dx, dy in zip(dxs,dys): # 상하좌우: 인접 체크
         nx,ny = x+dx, y+dy
-        if not in_range(nx,ny):
+        if not in_range(nx,ny): # 범위를 초과하면 pass
             continue
-        if visited[nx][ny]:
+        if visited[nx][ny]: # 이미 방문했으면 pass
             continue
-        if town[nx][ny]>k:
+        if town[nx][ny]>k: # 물에 잠기지 않은 집만 계속 탐색
             visited[nx][ny]=True
             dfs(nx,ny,k)
 
 total = []
 
-for k in range(1,101):
-    visited = [[False]*m for _ in range(n)]
+for k in range(1,101): # 비가 온 높이마다 마을 수 체크
+    visited = [[False]*m for _ in range(n)] # 방문 처리
     town_num =0
     for x in range(n):
         for y in range(m):
-            if visited[x][y]:
-                continue
-            if town[x][y]>k:
+            if not visited[x][y] and town[x][y]>k: # 방문하지 않았고 물에 잠기지 않았을 때
                 visited[x][y]=True
                 town_num+=1
                 dfs(x,y,k)
