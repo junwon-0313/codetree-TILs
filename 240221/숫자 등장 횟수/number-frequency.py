0d4d677e8cfb8d53020aaf4b49bb8773459de14b
Jo_on <jjuny9798@gmail.com>
2024-02-21 16:41:29 +0900

    Title: 숫자 등장 횟수, Time: 186ms, Memory: 34MB, Status: Wrong Answer - CodeTree

--- 240221/숫자 등장 횟수/number-frequency.py
+++ 240221/숫자 등장 횟수/number-frequency.py
@@ -3,5 +3,15 @@ n, m = map(int, input().split())
 num_lst = list(map(int, input().split()))
 cnt_lst = list(map(int, input().split()))
 
+d = dict()
+for num in num_lst:
+    if num not in d:
+        d[num]=1
+    else:
+        d[num]+=1
+
 for num in cnt_lst:
-    print(num_lst.count(num), end = ' ')
+    if num not in d:
+        print(0, end = '')
+    else:
+        print(d[num], end = ' ')
