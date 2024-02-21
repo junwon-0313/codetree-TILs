a27be1577d1142d38010512e58b2c136813f9695
Jo_on <jjuny9798@gmail.com>
2024-02-21 17:52:48 +0900

    Title: 세 수의 합, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240221/세 수의 합/sum-of-three-num.py
+++ 240221/세 수의 합/sum-of-three-num.py
@@ -1,4 +1,3 @@
-from itertools import combinations
 n, k = map(int,input().split())
 lst = list(map(int,input().split()))
 d = dict()
@@ -8,22 +7,23 @@ for num in lst:
     else:
         d[num]+=1
 
+d_k = list(d.keys())
 ans = 0
-# 세 수가 모두 같을 때
-if k%3==0 and d[k//3]>=3:
-    ans += (d[k//3]*(d[k//3]-1)* (d[k//3]-2))//6
-# 두 수가 같을 때
-for num in list(d.keys()):
-    if k-2*num ==num:
-        continue
-    if k-2*num in d:
-        ans += (d[num]*(d[num]-1))//2 * d[k-2*num]
-# 모두 다를 때
-for tmp in list(combinations(list(d.keys()), 3)):
-    if k == sum(tmp):
-        t = 1
-        for num in tmp:
-            t*=d[num]
-        ans +=t
+for num1 in d_k:
+    for num2 in d_k:
+        for num3 in d_k:
+            tmp = [num1,num2,num3]
+            if k ==sum(tmp):
+                if len(set(tmp))==1 and d[num1]>=3: # 세 수가 모두 같을 때
+                    ans += d[num1]*(d[num1]-1)*(d[num1]-2)//6
+                elif len(set(tmp))==2: # 두 수가 같을 때
+                    if num1==num2 and d[num1]>=2:
+                        ans +=d[num1]*(d[num1]-1)*d[num3]//2
+                    elif num2==num3 and d[num2]>=2:
+                        ans +=d[num2]*(d[num2]-1)*d[num1]//2
+                    elif num3==num1 and d[num1]>=2:
+                        ans +=d[num3]*(d[num3]-1)*d[num2]//2
+                elif len(set(tmp))==3: #모두 다를 때
+                    ans += d[num1]*d[num2]*d[num3]
 
 print(ans)
