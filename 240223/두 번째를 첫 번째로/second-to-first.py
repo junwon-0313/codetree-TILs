48a66f44d94f9375a2708ef8146a706d312af2de
Jo_on <jjuny9798@gmail.com>
2024-02-23 14:40:10 +0900

    Title: 두 번째를 첫 번째로, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240223/두 번째를 첫 번째로/second-to-first.py
+++ 240223/두 번째를 첫 번째로/second-to-first.py
@@ -1,7 +1,10 @@
 s = input()
 criteria = s[1]
 first = s[0]
-for idx in range(2,len(s)):
+new_s = s[:1]
+for idx in range(1,len(s)):
     if s[idx] == criteria:
-        s[idx]=first
-print(s)
+        new_s+= first
+    else:
+        new_s+=s[idx]
+print(new_s)
