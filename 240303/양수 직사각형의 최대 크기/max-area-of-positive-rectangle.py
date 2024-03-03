e97943bef37fbf4b3ad70c28aeb0a815e735750d
Jo_on <jjuny9798@gmail.com>
2024-03-03 20:57:59 +0900

    Title: 양수 직사각형의 최대 크기, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240303/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240303/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -5,12 +5,9 @@ graph = [list(map(int,input().split())) for _ in range(n)]
 ans = -1
 # 방향은 오른쪽 아래로만 가도 충분함, 중복됨
 # 시간 복잡도: O(n*m*n*m)
-# def in_range(x,y):
-#     return 0<=x<n and 0<=y<m
 def rectangle(x,y):
     global ans
-    # 몇번째 행 열까지 진행해도 되는지 체크
-    end_x, end_y = n,m
+    end_x, end_y = n,m # 몇번째 행 열까지 진행해도 되는지 체크
     for nx in range(x, n):
         if graph[nx][y]<=0:
             end_x=nx
@@ -19,26 +16,27 @@ def rectangle(x,y):
         if graph[x][ny]<=0:
             end_y=ny
             break
-    flag_x, flag_y = False, False
+    
+    flag_x, flag_y = False, False # 아래 반복문이 개별적으로 끝까지 동작하게 추가
     for nx in range(x,end_x): # 행을 고정
         for ny in range(y,end_y):
             if graph[nx][ny]<=0:
                 flag_x=True # 종료
-        if not flag_x:
-            rect_size = (nx-x+1)*(ny-y+1)
-            # print('행 고정',rect_size, nx,x,ny,y)
-            if rect_size>ans:
-                ans = rect_size
+            if not flag_x:
+                rect_size = (nx-x+1)*(ny-y+1)
+                # print('행 고정',rect_size, nx,x,ny,y)
+                if rect_size>ans:
+                    ans = rect_size
     
     for ny in range(y,end_y): # 열을 고정
         for nx in range(x,end_x): 
             if graph[nx][ny]<=0:
                 flag_y =True # 종료
-        if not flag_y:
-            rect_size = (nx-x+1)*(ny-y+1)
-            # print('열 고정',rect_size)
-            if rect_size>ans:
-                ans = rect_size
+            if not flag_y:
+                rect_size = (nx-x+1)*(ny-y+1)
+                # print('열 고정',rect_size)
+                if rect_size>ans:
+                    ans = rect_size
 
 # 시작점: 양수
 for x in range(n):
