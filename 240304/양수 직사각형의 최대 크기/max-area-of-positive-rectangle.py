bd4146be54ccfa57804eeebbb86c26542fc6a97c
Jo_on <jjuny9798@gmail.com>
2024-03-04 20:02:20 +0900

    Title: 양수 직사각형의 최대 크기, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 240304/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240304/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -6,19 +6,19 @@ graph = [list(map(int,input().split())) for _ in range(n)]
 ans = -1
 def rectangle(x,y): #(x,y)에서 (n,m)까지 탐색
     global ans
+    min_rect = 100 # 최댓값으로 초기화
     lst = [] # (x,y)를 시작하여 각 행에서 만들 수 있는 직사각형의 크기를 저장
+    cnt_x=0
     for nx in range(x,n):
-        cnt = 0
+        cnt_y = 0
+        cnt_x += 1
         for ny in range(y, m):
             if graph[nx][ny]>0:
-                cnt+=1
+                cnt_y+=1
             else:
                 break
-        lst.append(cnt) 
-    min_rect = 100 # 최댓값으로 초기화
-    for idx, s in enumerate(lst):
-        min_rect= min(min_rect,s) # 한 점을 시작으로 직사각형이 되려면 해당 조건을 따라야 함.
-        rect_size = min_rect*(idx+1) # 세로 x 가로
+        min_rect= min(min_rect,cnt_y) # 한 점을 시작으로 직사각형이 되려면 해당 조건을 따라야 함.
+        rect_size = min_rect*(cnt_x) # 세로 x 가로
         if ans<rect_size: # 최댓값 업데이트
             ans = rect_size
 
