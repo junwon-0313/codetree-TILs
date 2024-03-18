b944dc9f5f9e282b000ea2176a3bd5dae1d18f27
Jo_on <jjuny9798@gmail.com>
2024-03-18 18:28:26 +0900

    Title: 특정 수와 근접한 합, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240318/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240318/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -4,7 +4,7 @@ ans = sum(lst) # 최대값으로 설정
 # 전체 합에서 두 수를 뺀다. 그 결과와 s의 차이가 최소가 되게 한다. 
 total= sum(lst)
 for x in range(n):
-    for y in range(x, n):
+    for y in range(x+1, n):
         diff = abs(s - (total - lst[x]-lst[y]))
         if diff<ans:
             ans=diff
