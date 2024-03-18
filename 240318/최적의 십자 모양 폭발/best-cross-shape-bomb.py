fa0be2d6dc8a97c6bbe3f728211dceb661f57630
Jo_on <jjuny9798@gmail.com>
2024-03-18 21:06:21 +0900

    Title: 최적의 십자 모양 폭발, Time: 797ms, Memory: 30MB, Status: Passed - CodeTree

--- 240318/최적의 십자 모양 폭발/best-cross-shape-bomb.py
+++ 240318/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -6,45 +6,37 @@ for _ in range(n):
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
-# 폭탄을 터트림
-# def bomb(x,y):
-#     num= graph[x][y]-1
-#     for nx in range(x-num,x+num+1): # 열
-#         if in_range(nx,y):
-#             graph[nx][y]=-1
-#     for ny in range(y-num,y+num+1): # 행
-#         if in_range(x,ny):
-#             graph[x][ny]=-1
-def bomb_gravity(x,y,graph):
+def bomb(x,y,graph):
     num= graph[x][y]-1
-    y_cnt = 0
     for nx in range(x-num,x+num+1): # 열
         if in_range(nx,y):
             graph[nx][y]=-1
-            y_cnt+=1
     for ny in range(y-num,y+num+1): # 행
-        if ny==y:
-            continue
         if in_range(x,ny):
-            for nx in range(1,x):
-                graph[nx][ny] = graph[nx-1][ny]
-            graph[0][ny]=-1
-    # y에서 하나는 반드시 터짐 -> 여기를 보완
-    num_lst= [] 
-    for nx in range(n):
-        if graph[nx][y]==-1:
-            break
-        num_lst.append(graph[nx][y])
-    for nx in range(n):
-        if nx+1 <=y_cnt:
-            graph[nx][y]=-1
-        else:
-            break
-    for idx in range(len(num_lst)):
-        graph[y_cnt+idx][y]=num_lst[idx]
-        
-    return graph
+            graph[x][ny]=-1
+    return graph, num
 
+def gravity(graph, num):
+    for ny in range(y-num,y+num+1):
+        if in_range(x,ny):
+            # y에서 하나는 반드시 터짐 -> 여기를 보완
+            num_lst= []
+            y_cnt = 0
+            for nx in range(n):
+                if graph[nx][ny]==-1:
+                    y_cnt+=1
+            for nx in range(n):
+                if graph[nx][ny]==-1:
+                    break
+                num_lst.append(graph[nx][ny])
+            for nx in range(n):
+                if nx+1 <=y_cnt:
+                    graph[nx][ny]=-1
+                else:
+                    break
+            for idx in range(len(num_lst)):
+                graph[y_cnt+idx][ny]=num_lst[idx]
+    return graph
 
 def print_graph(graph):
     for x in range(n):
@@ -76,10 +68,12 @@ max_ans =-1
 for x in range(n):
     for y in range(n):
         tmp_graph = copy_graph(graph)
-        bomb_gravity(x,y,tmp_graph)
-        # print('AFTER BoMB', x,y)
+        tmp_graph,num = bomb(x,y,tmp_graph)
+        # print('AFTER BoMB+ gravity', x,y)
+        tmp_graph = gravity(tmp_graph, num)
         # print_graph(tmp_graph)
         tmp_cnt = find_couple(tmp_graph)
+        # print(tmp_cnt)
         if tmp_cnt>max_ans:
             max_ans=tmp_cnt
 print(max_ans)
