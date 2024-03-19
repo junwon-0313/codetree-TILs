f3c9ecfd6c40c1b2eb3be4935568b90a676b52e7
Jo_on <jjuny9798@gmail.com>
2024-03-19 23:13:27 +0900

    Title: 데이터 비교, Time: 189ms, Memory: 44MB, Status: Passed - CodeTree

--- 240319/데이터 비교/data-comparison.py
+++ 240319/데이터 비교/data-comparison.py
@@ -2,9 +2,11 @@ n = int(input())
 arr1 = list(map(int,input().split()))
 m = int(input())
 arr2 = list(map(int,input().split()))
-
+d = dict()
+for k in set(arr1):
+    d[k]=1
 for num in arr2:
-    if num in set(arr1):
+    if num in d:
         print(1, end = ' ')
     else:
         print(0, end =' ')
