538756e7587013570b9a7a25562effd97375a600
Jo_on <jjuny9798@gmail.com>
2024-03-27 17:34:48 +0900

    Title: 행복한 수열의 개수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,35 @@
+n,m = map(int,input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+ans =0
+# 행 마다
+for x in range(n):
+    max_cnt =0
+    cnt = 1
+    pre = graph[x][0]
+    for y in range(1,n):
+        if pre ==graph[x][y]:
+            cnt+=1
+        else:
+            pre = graph[x][y]
+            cnt=1
+        max_cnt = max(max_cnt, cnt)
+    if max_cnt>=m:
+        ans+=1
+# 열마다
+for y in range(n):
+    max_cnt =0
+    cnt = 1
+    pre = graph[0][y]
+    for x in range(1,n):
+        if pre ==graph[x][y]:
+            cnt+=1
+        else:
+            pre = graph[x][y]
+            cnt=1
+        max_cnt = max(max_cnt, cnt)
+    if max_cnt>=m:
+        ans+=1
+print(ans)
