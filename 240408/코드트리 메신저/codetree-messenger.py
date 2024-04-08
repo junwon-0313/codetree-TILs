20e8d4acfca50a75a7b43c8bbbf6665121ae41f9
Jo_on <jjuny9798@gmail.com>
2024-04-08 22:23:19 +0900

    Title: 코드트리 메신저, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240408/코드트리 메신저/codetree-messenger.py
+++ 240408/코드트리 메신저/codetree-messenger.py
@@ -16,15 +16,6 @@ alarm = [True for _ in range(n+1)] # 켜져있음 T
 
 def find_chat(c):
     # 한 부모의 자식은 다른 부모의 자식일 수 없으므로 방문 처리 필요없음
-    tree = dict()
-    for idx, p in enumerate(parent):
-        if idx ==0:
-            continue
-        if p in tree:
-            tree[p].append(idx)
-        else:
-            tree[p] = [idx]
-    # print(tree)
     cnt = 0
     child_lst = [(c,0)]
     while child_lst:
@@ -37,7 +28,7 @@ def find_chat(c):
                         cnt+=1
     return cnt
 
-for q in q_lst:
+for q in q_lst: # 10만
     if q.cmd ==100:
         idx =0
         for p in q.other[:n]:
@@ -47,6 +38,16 @@ for q in q_lst:
         for a in q.other[n:]:
             idx +=1
             authority[idx] = a
+
+        tree = dict()
+        for idx, p in enumerate(parent): # 10만 
+            if idx ==0:
+                continue
+            if p in tree:
+                tree[p].append(idx)
+            else:
+                tree[p] = [idx]
+        # print(tree)
         # print(parent, authority)
     elif q.cmd ==200:
         c = q.other[0]
@@ -60,7 +61,27 @@ for q in q_lst:
         authority[c]=power
     elif q.cmd ==400:
         c1,c2 = q.other
+        c1_lst = tree[parent[c1]]
+        c2_lst = tree[parent[c2]]
+        tmp1 = [c2]
+        tmp2 = [c1]
+        # print('CHANGE')
+        # print(c1,c2)
+        # print('BEFORE')
+        # print(tree[parent[c1]],tree[parent[c2]])
+        for num in c1_lst:
+            if c1 ==num:
+                continue
+            tmp1.append(num)
+        for num in c2_lst:
+            if c2 ==num:
+                continue
+            tmp2.append(num)
         parent[c1], parent[c2] = parent[c2], parent[c1]
+        tree[parent[c1]], tree[parent[c2]] = tmp1, tmp2
+        # print('AFTER')
+        # print(tree[parent[c1]],tree[parent[c2]])
+
     elif q.cmd ==500:
         c = q.other[0]
         ans = find_chat(c)
